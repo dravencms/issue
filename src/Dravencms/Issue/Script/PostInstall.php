6c25f9e7b925a5b66769b167c92ea5c85d3491c2
Adam Schubert <adam.schubert@sg1-game.net>
2016-11-26 16:22:13 +0100

    Install script

--- src/Dravencms/Issue/Script/PostInstall.php
+++ src/Dravencms/Issue/Script/PostInstall.php
@@ -6,6 +6,8 @@ use Dravencms\Model\Admin\Entities\Menu;
 use Dravencms\Model\Admin\Repository\MenuRepository;
 use Dravencms\Model\User\Entities\AclOperation;
 use Dravencms\Model\User\Entities\AclResource;
+use Dravencms\Model\User\Repository\AclOperationRepository;
+use Dravencms\Model\User\Repository\AclResourceRepository;
 use Dravencms\Packager\IPackage;
 use Dravencms\Packager\IScript;
 use Kdyby\Doctrine\EntityManager;
@@ -15,28 +17,58 @@ use Kdyby\Doctrine\EntityManager;
  */
 class PostInstall implements IScript
 {
+    /** @var MenuRepository */
     private $menuRepository;
+
+    /** @var EntityManager */
     private $entityManager;
 
-    public function __construct(MenuRepository $menuRepository, EntityManager $entityManager)
+    /** @var AclResourceRepository */
+    private $aclResourceRepository;
+
+    /** @var AclOperationRepository */
+    private $aclOperationRepository;
+
+    /**
+     * PostInstall constructor.
+     * @param MenuRepository $menuRepository
+     * @param EntityManager $entityManager
+     * @param AclResourceRepository $aclResourceRepository
+     * @param AclOperationRepository $aclOperationRepository
+     */
+    public function __construct(MenuRepository $menuRepository, EntityManager $entityManager, AclResourceRepository $aclResourceRepository, AclOperationRepository $aclOperationRepository)
     {
         $this->menuRepository = $menuRepository;
         $this->entityManager = $entityManager;
+        $this->aclResourceRepository = $aclResourceRepository;
+        $this->aclOperationRepository = $aclOperationRepository;
     }
 
+    /**
+     * @param IPackage $package
+     */
     public function run(IPackage $package)
     {
-        $aclResource = new AclResource('issue', 'Issue');
+        if (!$aclResource = $this->aclResourceRepository->getOneByName('issue')) {
+            $aclResource = new AclResource('issue', 'Issue');
 
-        $this->entityManager->persist($aclResource);
+            $this->entityManager->persist($aclResource);
+        }
 
-        $aclOperationEdit = new AclOperation($aclResource, 'edit', 'Allows editation of Issue');
-        $this->entityManager->persist($aclOperationEdit);
-        $aclOperationDelete = new AclOperation($aclResource, 'delete', 'Allows deletion of Issue');
-        $this->entityManager->persist($aclOperationDelete);
+        if (!$aclOperationEdit = $this->aclOperationRepository->getOneByName('edit')) {
+            $aclOperationEdit = new AclOperation($aclResource, 'edit', 'Allows editation of Issue');
+            $this->entityManager->persist($aclOperationEdit);
+        }
 
-        $adminMenuRoot = new Menu('Issues', ':Admin:Issue:Issue', 'fa-bug', $aclOperationEdit);
-        $this->entityManager->persist($adminMenuRoot);
+        if (!$aclOperationDelete = $this->aclOperationRepository->getOneByName('delete')) {
+            $aclOperationDelete = new AclOperation($aclResource, 'delete', 'Allows deletion of Issue');
+            $this->entityManager->persist($aclOperationDelete);
+        }
+
+        if (!$this->menuRepository->getOneByPresenter(':Admin:Issue:Issue')) {
+            $adminMenuRoot = new Menu('Issues', ':Admin:Issue:Issue', 'fa-bug', $aclOperationEdit);
+            $this->entityManager->persist($adminMenuRoot);
+        }
 
     }
 }
