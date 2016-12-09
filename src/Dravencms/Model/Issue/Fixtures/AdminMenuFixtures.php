5daa808aff5995dee682fb440cf1412a311cade4
Adam Schubert <adam.schubert@sg1-game.net>
2016-12-09 01:55:47 +0100

    Namespace fix

--- src/Dravencms/Model/Issue/Fixtures/AdminMenuFixtures.php
+++ src/Dravencms/Model/Issue/Fixtures/AdminMenuFixtures.php
@@ -3,7 +3,7 @@
  * Copyright (C) 2016 Adam Schubert <adam.schubert@sg1-game.net>.
  */
 
-namespace Dravencms\Model\Structure\Fixtures;
+namespace Dravencms\Model\Issue\Fixtures;
 
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Dravencms\Model\Admin\Entities\Menu;
